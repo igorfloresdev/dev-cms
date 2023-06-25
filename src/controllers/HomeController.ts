fb14f68e1c15440e3cb107bc1c3c8d6b46d6c9c2
igorfloresdev <dev.igorflores@gmail.com>
2023-06-25 16:36:40 -0300

    Home controller created

--- src/controllers/HomeController.ts
+++ src/controllers/HomeController.ts
@@ -1,20 +1,37 @@
-import { Request, Response } from 'express'
+import { PrismaClient } from '@prisma/client'
+import { NextFunction, Request, Response } from 'express'
 
+const prisma = new PrismaClient()
+const home = prisma.parameters
 class HomeController {
-  static async create(req: Request, res: Response) {
-    res.status(200).send('create home')
+  static async update(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { title, subtitle, imageUrl } = req.body
+
+      const result = await home.findFirst({
+        select: {
+          id: true,
+        },
+      })
+
+      await home.update({
+        where: { id: result?.id },
+        data: { homeTitle: title, homeSubtitle: subtitle, homeImageUrl: imageUrl },
+      })
+
+      res.status(200).json({ message: 'Home section updated successfully' })
+    } catch (error) {
+      next(error)
+    }
   }
 
-  static async read(req: Request, res: Response) {
-    res.status(200).send('get home')
-  }
-
-  static async update(req: Request, res: Response) {
-    res.status(200).send('update home')
-  }
-
-  static async delete(req: Request, res: Response) {
-    res.status(200).send('delete home')
+  static async read(req: Request, res: Response, next: NextFunction) {
+    try {
+      const result = await home.findFirst()
+      res.status(200).json(result)
+    } catch (error) {
+      next(error)
+    }
   }
 }
 
