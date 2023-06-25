9be705752fd01e6ee7f9170f2884f49bc874568a
igorfloresdev <dev.igorflores@gmail.com>
2023-06-25 16:37:12 -0300

    changed home route

--- src/routes/home.ts
+++ src/routes/home.ts
@@ -3,9 +3,7 @@ import HomeController from '../controllers/HomeController'
 
 const router = express.Router()
 
-router.post('/home', HomeController.create)
 router.get('/home', HomeController.read)
 router.put('/home', HomeController.update)
-router.delete('/home', HomeController.delete)
 
 export default router
